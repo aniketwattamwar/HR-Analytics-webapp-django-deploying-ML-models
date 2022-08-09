db36e53fc987fab1673933055c41975cc1698973
Arivald8 <arivald88@gmail.com>
2022-08-09 12:08:42 +0100

    Refactored app/views.py models function. The function now adheres to DRY principles and does not repeat redundant code. All conditional statements removed from the function as it can handle multiple classifiers without conditionals.

--- app/views.py
+++ app/views.py
@@ -10,44 +10,33 @@ test_data_preprocessed = pd.read_csv('test_preprocessed.csv')
 test_data_preprocessed = test_data_preprocessed.drop(['Unnamed: 0'],axis =1)
 test_data_preprocessed = test_data_preprocessed.iloc[:,:].values
 
+valid_requests = {
+    'gNB': ('gNB.sav', 'guassianNB.csv'),
+    'multiNB': ('classifier_multi_NB.sav', 'multi_NB.csv'),
+    'rf': ('multi_NB.csv', 'rf.csv')
+}
+
+
 def home(request):
     return  render(request,"index.html")
 
-def models(request):
-        
-    if 'gNB' in request.POST:
-        gaussian = pickle.load(open('gNB.sav','rb'))
-        y_pred = gaussian.predict(test_data_preprocessed)
-        output = pd.DataFrame(y_pred)
-        output.to_csv('gaussianNB.csv')
-        
-        filename = 'gaussianNB.csv'
-        response = HttpResponse(open(filename, 'rb').read(), content_type='text/csv')               
-        response['Content-Length'] = os.path.getsize(filename)
-        response['Content-Disposition'] = 'attachment; filename=%s' % 'gaussianNB.csv'
-        return response
-    
-    if 'multiNB' in request.POST:
-        multi = pickle.load(open('classifier_multi_NB.sav','rb'))
-        y_pred = multi.predict(test_data_preprocessed)
-        output = pd.DataFrame(y_pred)
-        output.to_csv('multi_NB.csv')
-        
-        filename = 'multi_NB.csv'
-        response = HttpResponse(open(filename, 'rb').read(), content_type='text/csv')                
-        response['Content-Length'] = os.path.getsize(filename)
-        response['Content-Disposition'] = 'attachment; filename=%s' % 'multi_NB.csv'
-        return response
-    
-    if 'rf' in request.POST:
-        rf = pickle.load(open('random_forest.sav','rb'))
-        y_pred = rf.predict(test_data_preprocessed)
-        output = pd.DataFrame(y_pred)
-        output.to_csv('rf.csv')
-        
-        filename = 'rf.csv'
-        response = HttpResponse(open(filename, 'rb').read(), content_type='text/csv')             
-        response['Content-Length'] = os.path.getsize(filename)
-        response['Content-Disposition'] = 'attachment; filename=%s' % 'rf.csv'
-        return response
 
+def models(request):
+    length = os.path.getsize(valid_requests[request.POST])
+    dispos = 'attachment; filename=%s' % f'{valid_requests[request.POST][1]}'
+
+    pd.DataFrame(
+        pickle.load(
+            open(valid_requests[request.POST][0])
+        ).predict(test_data_preprocessed)
+    ).to_csv(valid_requests[request.POST])
+
+    response = HttpResponse(open(
+        valid_requests[request.POST], 'rb').read(),
+        content_type="text/csv"
+    )
+
+    response['Content-Length'] = length
+    response['Content-Disposition'] = dispos
+
+    return response
